243aa16ea9ddc52082a49390c08ae4a6796ee2a3
chrismckelt <chris@mckelt.com>
2026-07-09 22:39:35 +0000

    format: clean up whitespace in server.py

--- scripts/server.py
+++ scripts/server.py
@@ -3,9 +3,11 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 class DocsHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=ROOT_DIR, **kwargs)
+
 
 if __name__ == "__main__":
     HTTPServer.allow_reuse_address = True
